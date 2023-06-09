6882973b69585f59baa870b99c98210c87f54669
Leschinskiy.AV <Leschinskiy.AV@dns-shop.ru>
2023-06-09 23:18:20 +1000

    added task1

---
+++ SkillFactory86/Program.cs
@@ -0,0 +1,47 @@
+﻿
+Console.WriteLine("Enter folder path:");
+string path = Console.ReadLine();
+DirectoryInfo dirInfo = new DirectoryInfo(path);
+if (dirInfo.Exists)
+{
+    DisposeUnusedFiles(dirInfo);
+}
+else
+{
+    Console.WriteLine("This folder does not exist!");
+}
+
+void DisposeUnusedFiles(DirectoryInfo path)
+{
+    foreach (var directory in path.GetDirectories())
+    {
+        DisposeUnusedFiles(directory);
+        
+        try
+        {
+            foreach (var file in directory.GetFiles())
+            {
+                if (DateTime.Now - file.LastAccessTime > TimeSpan.FromMinutes(1))
+                {
+                    {
+                        file.Delete();
+                    }
+                }
+            }
+
+            foreach (var dir in directory.GetDirectories())
+            {
+                if (DateTime.Now - dir.LastAccessTime > TimeSpan.FromMinutes(1))
+                {
+                    {
+                        dir.Delete(true);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
