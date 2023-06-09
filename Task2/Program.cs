23d3bf3e5a1d84436292a721f4232c57140c13cc
Leschinskiy.AV <Leschinskiy.AV@dns-shop.ru>
2023-06-10 08:53:39 +1000

    добавил таск2 и тестовые данные

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("Enter folder path:");
+string path = Console.ReadLine();
+DirectoryInfo dirInfo = new DirectoryInfo(path);
+if (dirInfo.Exists)
+{
+    long size = 0;
+    GetFolderSize(dirInfo, ref size);
+    Console.WriteLine(size.ToString());
+}
+else
+{
+    Console.WriteLine("This folder does not exist!");
+}
+
+void GetFolderSize(DirectoryInfo path, ref long size)
+{
+    try
+    {
+        FileInfo[] fis = path.GetFiles();
+        foreach (FileInfo fi in fis)
+        {
+            size += fi.Length;
+            //для тестов в директории проекта есть папка FolderForTest
+            //Console.WriteLine($"file:{fi.FullName}, size:{fi.Length}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+
+    foreach (var directory in path.GetDirectories())
+    {
+        GetFolderSize(directory, ref size);
+    }
+}
